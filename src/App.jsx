18a9c82f8c905c9c9985c1ba2106b5ca292c0f22
Carlos <c4r1oszambrano@gmail.com>
2025-01-02 17:05:41 -0500

    Linterns fixed
    Linterns fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Mainpage from './Components/Homepage';
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -2,13 +2,13 @@
 import React from 'react';
 
 const Homepage = () => {
-    return (
-        <header>
-            <h1>GTH</h1>
-            <h2>Especialistas</h2>
-            <h2>¿Quiénes somos?</h2>
-        </header>
-    );
+  return (
+    <header>
+      <h1>GTH</h1>
+      <h2>Especialistas</h2>
+      <h2>¿Quiénes somos?</h2>
+    </header>
+  );
 };
 
 export default Homepage;
