122e2e90bf875897829aa65899d4582856368dae
Carlos <c4r1oszambrano@gmail.com>
2025-01-03 19:10:27 -0500

    Feature:
    Creating body and heder

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,13 +1,26 @@
-/* eslint linebreak-style: ["error", "windows"] */
 import React from 'react';
+import '../stylesheets/homepage.css';
 
 const Homepage = () => {
   return (
-    <header>
-      <h1>GTH</h1>
-      <h2>Especialistas</h2>
-      <h2>¿Quiénes somos?</h2>
-    </header>
+    <div className="Home-Page">
+      <header className="Home-Roof">
+        <div className="Home-txt"><h1>GTH</h1></div>
+        <div className="Home-windows">
+          <h2>Especialistas</h2>
+          <h2>¿Quiénes somos?</h2>
+        </div>
+        <div>
+          <button type="submit">
+            <h3>Loguearse</h3>
+          </button>
+          <button type="submit">
+            <h3>Crear Cuenta</h3>
+          </button>
+        </div>
+      </header>
+      <div className="Home-body" />
+    </div>
   );
 };
 
