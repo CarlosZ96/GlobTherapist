6842a2d0438b167ec385358b8d81accc4f2b1c3f
Carlos <c4r1oszambrano@gmail.com>
2025-01-31 18:02:28 -0500

    Update useMonthData.jsx
    Spanish months updated

--- src/hooks/useMonthData.jsx
+++ src/hooks/useMonthData.jsx
@@ -6,38 +6,31 @@ import {
   addMonths,
   format,
 } from 'date-fns';
+import { es } from 'date-fns/locale';
 
-// Hook principal
 const useMonthData = () => {
-  const [currentDate, setCurrentDate] = useState(new Date()); // Fecha actual
-  const [days, setDays] = useState([]); // Días del mes
-  const [monthName, setMonthName] = useState(''); // Nombre del mes
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [days, setDays] = useState([]);
+  const [monthName, setMonthName] = useState('');
 
   useEffect(() => {
-    // Obtener el primer día del mes
     const firstDayOfMonth = startOfMonth(currentDate);
-
-    // Obtener la cantidad de días en el mes
     const daysInMonth = getDaysInMonth(currentDate);
-
-    // Obtener el día de la semana en que comienza el mes (0 = Domingo, 6 = Sábado)
     const startDay = getDay(firstDayOfMonth);
 
-    // Crear un array de días para el mes actual
     const emptyDays = Array.from({ length: daysInMonth }, (_, index) => ({
       date: index + 1,
-      active: false, // Puedes cambiar esto según tus necesidades
+      active: false,
     }));
 
-    // Rellenar los días anteriores al primer día del mes con `null`
     const paddedDays = Array(startDay).fill(null).concat(emptyDays);
     setDays(paddedDays);
 
-    // Obtener el nombre del mes
-    setMonthName(format(currentDate, 'MMMM yyyy'));
+    // Formatear el nombre del mes en español y en minúsculas
+    const formattedMonthName = format(currentDate, 'MMMM yyyy', { locale: es });
+    setMonthName(formattedMonthName.toLowerCase()); // Convertir a minúsculas
   }, [currentDate]);
 
-  // Función para cambiar el mes
   const changeMonth = (offset) => {
     setCurrentDate((prevDate) => addMonths(prevDate, offset));
   };
