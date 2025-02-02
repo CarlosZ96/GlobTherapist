4d4cfe07a3e8d4375329bcbd5ac48d8475de59cf
Carlos <c4r1oszambrano@gmail.com>
2025-02-01 19:16:17 -0500

    Update ProData.jsx
    Se agregaron los campos para  la información del pro

--- src/Components/ProData.jsx
+++ src/Components/ProData.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import User from '../img/user.png';
 import '../stylesheets/prospace.css';
 
 const ProData = () => {
@@ -6,6 +7,25 @@ const ProData = () => {
     <div className="prodata-cont">
       <div>
         <h1>Mis datos</h1>
+      </div>
+      <div className="data-cont">
+        <div className="pro-name-cont">
+          <div className="user-image-comt">
+            <img src={User} alt="user" className="pro-img" />
+          </div>
+          <h2 className="pro-name">pro name</h2>
+        </div>
+        <div className="pro-personal-info">
+          <div className="hdv-cont">
+            <h3 className="upload-hdv">Subir HDV</h3>
+          </div>
+          <div>
+            <h3>Información personal</h3>
+            <p>Nombre: </p>
+            <p>Email: </p>
+            <p>Teléfono: </p>
+          </div>
+        </div>
       </div>
     </div>
   );
